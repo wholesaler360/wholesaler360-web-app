56366ce71eee74d48e83b6c8904baa6f84b011fc
Sharad Barad <131551383+sharadbarad@users.noreply.github.com>
2025-01-14 10:16:39 +0530

    restructure

--- backend/api/login/login-route.js
+++
@@ -1,9 +0,0 @@
-import {login} from './login-controller.js';
-import Router from 'express';
-import {upload} from '../../middlewares/multer-middleware.js'
-
-const authRouter = Router();
-
-authRouter.route('/').post(upload.none(), login);
-
-export default authRouter;
--- backend/utils/asyncHandler-utils.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (requestHandler)=>{
-    return (req,res,next) =>{
-        // It wraps the request inside the Promise so that it could be handled better
-        Promise.resolve(requestHandler(req,res,next))
-        .catch((err)=>next(err))
-    }
-}
-
-export {asyncHandler}   
