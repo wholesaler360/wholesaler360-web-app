56e097491fb6ed9cb7b3f6a109797950df261b8b
Ujas Makwana <132668325+UjasMakwana9@users.noreply.github.com>
2025-01-16 11:11:14 +0530

    changes regarding structure

--- backend/src/product-category/product-category-controller.js
+++
@@ -1,8 +0,0 @@
-import { ApiError } from "../../utils/api-error-utils";
-import { ApiResponse } from "../../utils/api-Responnse-utils";
-import { asyncHandler } from "../../utils/asyncHandler-utils";
-import {Category} from './product-category-model.js'
-
-const createCategory = asyncHandler(async(req,res,next)=>{
-    
-});
