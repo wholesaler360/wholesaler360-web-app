798b0c296cf3a8518aa6652d5a8f6153fa6b213e
Priyanshu Baraiya <priyanshuahir01@gmail.com>
2025-01-17 08:42:57 +0530

    Add DataTable component and enhance Tailwind CSS configuration; update dependencies

---
+++ frontend/src/components/DataTable.jsx
@@ -0,0 +1,65 @@
+import { cn } from "@/lib/utils";
+import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from "./ui/table";
+import { flexRender } from "@tanstack/react-table";
+
+export function DataTable({ table, children, classNames, ...props }) {
+  return (
+    <div
+      className={cn("w-full space-y-2.5 overflow-auto", classNames)}
+      {...props}
+    >
+      {children}
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => {
+              return ( // Add return statement here
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead
+                        key={header.id}
+                        colSpan={header.colSpan}
+                        style={{
+                        opacity: 0.97,
+                        zIndex: 1,
+                        width: `150px`,
+                        }}
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                  {row.getVisibleCells().map((cell)=>(
+                    <TableCell key={cell.id} style={{}}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
+                  No results
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
