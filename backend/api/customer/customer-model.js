480883bf561703d16ddf5ae21ab68b011064edd7
Ujas Makwana <makwanaujas99@gmail.com>
2025-02-02 11:05:45 +0530

    fix: change pincode type from Number to String in customer model

--- backend/api/customer/customer-model.js
+++ backend/api/customer/customer-model.js
@@ -82,7 +82,7 @@ const customerSchema = new mongoose.Schema({
             lowercase: true,
         },
         pincode : {
-            type: Number,
+            type: String,
             required: true,
         },
     },
