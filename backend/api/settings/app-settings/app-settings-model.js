5e6fc5856722e7199b3a899f6f979356b8fb49bd
Sharad Barad <131551383+sharadbarad@users.noreply.github.com>
2025-02-18 15:14:19 +0530

    Add invoice template schema to app settings model

--- backend/api/settings/app-settings/app-settings-model.js
+++ backend/api/settings/app-settings/app-settings-model.js
@@ -27,6 +27,32 @@ const appEmailSettingsSchema = new Schema(
     },
     { timestamps: true }
 );
+const appInvoiceTemplateSchema = new Schema(
+    {
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            required: true,
+        },
+        credential: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        smtpHost: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        smtpPort: {
+            type: Number,
+            trim: true,
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
 
 
 export const AppEmailSettings = mongoose.model("CompanyEmailSettings", appEmailSettingsSchema);
