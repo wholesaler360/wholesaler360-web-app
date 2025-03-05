6a024034e79ed76cc22891a4c230d619844b94ca
Sharad Barad <131551383+sharadbarad@users.noreply.github.com>
2025-03-05 16:30:38 +0530

    feat: add fetchAll endpoint to retrieve invoices with customer details

--- backend/api/invoice/invoice-route.js
+++ backend/api/invoice/invoice-route.js
@@ -1,10 +1,11 @@
-import { createInvoice } from "./invoice-controller.js";
+import { createInvoice, fetchAll } from "./invoice-controller.js";
 import Router from 'express'
 
 const invoiceRouter = Router()
 
 
 invoiceRouter.route('/create').post(createInvoice)
+invoiceRouter.route('/fetchAll').get(fetchAll)
 
 
 export { invoiceRouter }
